67c22cd5c64c90e05357a029661bb6c9887e49d9
IamTheRafsane <sending.to.evan@gmail.com>
2024-11-19 01:55:47 +0600

    Form Validation Completed

--- process.php
+++ process.php
@@ -1,64 +1,106 @@
 <?php
 
-//Full Name
-if(isset($_POST['fullName'])){
-    $StudentName = $_POST['fullName'];
-    
-}
-else{echo"NO Result";}
+if (!empty($_POST['fullName']) && 
+    !empty($_POST['id']) && 
+    !empty($_POST['email']) && 
+    !empty($_POST['books']) && 
+    !empty($_POST['token']) && 
+    !empty($_POST['borrowDate']) && 
+    !empty($_POST['returnDate'])) {
 
-//Id
-if(isset($_POST['id'])){
-    $StudentId = $_POST['id'];
-    
-}
-else{echo"NO Result";}
+        if(isset($_POST['fullName']) &&
+        isset($_POST['id']) &&
+        isset($_POST['email']) && 
+        isset($_POST['books']) && 
+        isset($_POST['token']) && 
+        isset($_POST['borrowDate']) && 
+        isset($_POST['returnDate']))
+        {
+            //Student Name
+            if(preg_match("/^[a-zA-Z. ]*$/", $_POST['fullName'])){
+            $StudentName = ucwords($_POST['fullName']);
+            }
+            else{
+                echo "Error. Name only contains A-Z and dots"; exit;
+            }  
+            
 
-//E-mail
-if(isset($_POST['email'])){
-    $StudentEmail = $_POST['email'];
-    
-}
-else{echo"NO Result";}
+            //Student ID
+            if (preg_match("/^\d{2}-\d{5}-\d{1}$/", $_POST['id'])) {
+            $StudentId = $_POST['id'];
+            } else {
+                echo "Error. ID must be in the **-*****-* format.";
+                exit;
+            }
 
-//Choosen Book
-if(isset($_POST['books'])){
-    $BookName = $_POST['books'];
-    
-}
-else{echo"NO Result";}
 
-//Token
-if(isset($_POST['token'])){
-    $Token = $_POST['token'];
-    
-}
-else{echo"NO Result";}
+            //Student Email
+            if(preg_match("/^\d{2}-\d{5}-\d{1}@student\.aiub\.edu$/", $_POST['email'])){
+            $StudentEmail = $_POST['email'];
+            } 
+            else{
+                echo "Error. Must put aiub student email";
+                exit;
+            }
 
-//Borrow Date
-if(isset($_POST['borrowDate'])){
-    $BorrowDate = $_POST['borrowDate'];
-    
-}
-else{echo"NO Result";}
 
-//Return Date
-if(isset($_POST['returnDate'])){
-    $ReturnDate = $_POST['returnDate'];
-}
-else{echo"NO Result";}
+            //Token
+            if(preg_match("/^[0-9]*$/", $_POST['token'])){
+                $Token = $_POST['token'];
+            }
+            else{
+                echo "Error. Token must contain numbers only.";
+                exit;
+            }
 
-echo "<div style='width: 400px; margin: 0 auto; padding:50px; border:1px solid #333; font-family: Arial, sans-serif;'>";
-echo "<h2 style='text-align: center; font-size: 0.9em; color:#666;'> Thank you for borrowing the book</h2>";
-echo "<hr style ='border: 1px solid #333;'><br>";
-echo "<p>Full Name:  $StudentName</p>";
-echo "<p>Student ID:  $StudentId</p>";
-echo "<p>Student Email:  $StudentEmail</p>";
-echo "<p>Book Name:  $BookName</p>";
-echo "<p>Token No.:  $Token</p>";
-echo "<p>Borrow Date:  $BorrowDate</p>";
-echo "<p>Return Date:  $ReturnDate</p>";
+            //Book Name
+            $BookName = $_POST['books'];
 
-echo "</div>";
+
+            //Number of days borrowed
+            $BorrowDate = $_POST['borrowDate'];
+            $ReturnDate = $_POST['returnDate'];
+        
+            $date1 = date_create($BorrowDate);
+            $date2 = date_create($ReturnDate);
+            
+            $interval = date_diff($date1, $date2);
+
+            if($interval->days > 10 || $interval->days < 0){
+                echo "Date Error! Books can be borrowed for only 10 days.";
+                exit;
+            }
+            else {
+
+
+                //Print if everything is ok.
+                echo "Borrow Successfull.";
+
+                echo "<div style='width: 400px; margin: 0 auto; padding:50px; border:1px solid #333; font-family: Arial, sans-serif;'>";
+                echo "<h2 style='text-align: center; font-size: 0.9em; color:#666;'> Thank you for borrowing the book</h2>";
+                echo "<hr style ='border: 1px solid #333;'><br>";
+                echo "<p>Full Name:  $StudentName</p>";
+                echo "<p>Student ID:  $StudentId</p>";
+                echo "<p>Student Email:  $StudentEmail</p>";
+                echo "<p>Book Name:  $BookName</p>";
+                echo "<p>Token No.:  $Token</p>";
+                echo "<p>Borrow Date:  $BorrowDate</p>";
+                echo "<p>Return Date:  $ReturnDate</p>";
+                echo "</div>";
+
+            }
+
+
+        }
+        else{
+            echo "Input Error.";
+        }
+
+
+    }
+    else{
+        echo "Empty Fields.";
+    }
+
 
 ?>
